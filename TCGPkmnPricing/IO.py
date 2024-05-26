2a77a30ef4a1e8520a8797b7871bc31b57de1400
YasserKhaloufi <yasserkhaloufi2@gmail.com>
2024-05-26 15:51:14 +0200

    Fixato problema tabbing dei messaggi su discord, spostato metodo di frammentazione messaggi in IO

--- TCGPkmnPricing/IO.py
+++ TCGPkmnPricing/IO.py
@@ -8,6 +8,7 @@ from PIL import Image
 from requests import *
 from io import BytesIO
 
+# TERMINALE
 def askForPkmnName() -> str:
     return input("Insert Pokemon name:")
 
@@ -21,10 +22,6 @@ def printCardsDisplayInfo(cards) -> None:
     print(tabulate([[card.name, card.id, card.set['releaseDate']] for card in cards], headers=['Name', 'ID', 'Release Date']))
     # [print(f"Name:{getattr(card, 'name')} ID:{getattr(card, 'id')}\n") for card in cards]
     
-def getCardsDisplayInfo(cards) -> str:
-    str = tabulate([[card.name, card.id, card.set['releaseDate']] for card in cards], headers=['Name', 'ID', 'Release Date'])
-    return str
-    
 def getCardFromListByID(cards, ID) -> Card:
     for card in cards:
         if card.id == ID:
@@ -35,3 +32,40 @@ def displayImage(URL) -> None:
     response = get(URL)
     img = Image.open(BytesIO(response.content))
     img.show()
+    
+# DISCORD
+def getCardsDisplayInfo(cards) -> str:
+    str = tabulate([[card.name, card.id, card.set['releaseDate']] for card in cards], headers=['Name', 'ID', 'Release Date'])
+    return str
+    
+# Per gestione limite 2000 caratteri per messaggio
+def fragmentResponse(response) -> list[str]:
+    # Suddividi il messaggio in righe
+    lines = response.split('\n')
+    
+    # Crea una lista per contenere i vari messaggi
+    messages = []
+    message = "```" # Inizia ogni messaggio con backticks
+    
+    # Aggiungi righe al messaggio corrente finché non raggiungi il limite di 2000 caratteri
+    for line in lines:
+        # Se aggiungendo una nuova riga al messaggio superi i 2000 caratteri
+        if len(message) + len(line) + 4 > 2000:  # (+1 per il carattere '\n', + 3 per il backticks)
+            # Allora fermati e inizia a costruire il prossimo
+            message += "```" # Chiudi il messaggio corrente
+            messages.append(message) 
+            message = "```"
+        message += line + "\n"
+
+    # Aggiungi l'ultima parte del messaggio se non è vuota (per quando sono finite le linee, ma l'ultima rimane fuori)
+    if message:
+        message += "```"  # Chiudi l'ultimo messaggio con backticks
+        messages.append(message)
+        
+    return messages
+            
+    """ 
+        Perchè i backticks?
+        Discord utilizza un font monospazio per il testo formattato come codice (racchiuso tra backticks), ma per il testo normale utilizza un font proporzionale, in cui diversi caratteri possono avere larghezze diverse, questo mi dava problemi con l'allineamento del testo (gli spazi vuoti apparivano su discord meno larghi rispetto a come li vedevo in debugging) in sintesi, in una normale chat di discord era praticamente illeggibile (i vari campi erano sfasati).
+        Il workaround è l'uso del formato di codice di Discord, abilitato nel momento in cui il messaggio è racchiuso tra ```.
+    """
--- TCGPkmnPricing/bot.py
+++ TCGPkmnPricing/bot.py
@@ -1,8 +1,13 @@
+# ext
 import discord
 from discord.ext import commands
+
+# personali
 from tcgAPI_Interface import getCardByPkmnName
-from IO import getCardsDisplayInfo
 from secret import DISCORD_KEY
+from IO import getCardsDisplayInfo
+from IO import fragmentResponse
+
 
 # il bot deve ricevere eventi relativi ai contenuti dei messaggi.
 intents = discord.Intents.default()
@@ -27,28 +32,7 @@ async def Get(ctx): # ctx sta per context e contiene tutte le info della convers
     
     if cards != []:
         response = getCardsDisplayInfo(cards)
-        
-        # Gestione limite 2000 caratteri per messaggio
-        # Suddividi il messaggio in righe
-        lines = response.split('\n')
-        
-        # Crea una lista per contenere i vari messaggi
-        messages = []
-        message = ""
-        
-        # Aggiungi righe al messaggio corrente finché non raggiungi il limite di 2000 caratteri
-        for line in lines:
-            # Se aggiungendo una nuova riga al messaggio superi i 2000 caratteri
-            if len(message) + len(line) + 1 > 2000:  # (+1 per il carattere '\n')
-                # Allora fermati e inizia a costruire il prossimo
-                messages.append(message) 
-                message = ""          
-            message += line + "\n"
-
-        # Aggiungi l'ultima parte del messaggio se non è vuota (per quando sono finite le linee, ma l'ultima rimane fuori)
-        if message:
-            messages.append(message)
-
+        messages = fragmentResponse(response)
         # Invia ogni parte come un messaggio separato
         for m in messages:
             await ctx.send(m)
